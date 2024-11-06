f942a4ea1e8e931b34e2604f530f804adae8bc24
eshon <eshaan@Laptop-Probably.local>
2024-11-06 10:08:35 -0600

    added fill function to pseudocode

--- trirender.pseudo.py
+++ trirender.pseudo.py
@@ -1,8 +1,11 @@
-fn triRender(p1:array, p2:array, p3:array){
+fn triRender(p1:array, p2:array, p3:array, COLOR:string, FILL:bool){
 
   # all verticies translated to 2D, so each array has a length of y2-y1
   # assuming a setPixel() function preset in the programming language (even 8086 assembly has one)
   # file uses python file extension for correct code coloring in most editors
+
+  xs = (list)[]
+  ys = (list)[]
 
   fn lineEQ(x1, y1, x2, y2){
     m = (y2 - y1) / (x2 - x1)
@@ -22,8 +25,32 @@ fn triRender(p1:array, p2:array, p3:array){
     float[] domain = {xmin,xmax}
 
     for (i = xmin; i < (xmax+1); i++){
-      y = (m*i) + b
-      setPixel(estimate(i),estimate(y))
+      y = (m*i) + b 
+      ys.add(y)
+      xs.add(i)  
+      setPixel(estimate(i),estimate(y), COLOR)
+    }
+  }
+  fn fill(){
+    xs.remove(p1[0],p2[0],p3[0])
+    ys.remove(p1[1],p2[1],p3[1])
+    for (int i = 0; i < len(xs); i++){
+      xv = xs[i]
+      yv1 = ys[i]
+      xs.remove(xv)
+      ys.remove(yv1)
+      xj = 0
+      while(xs[xj] != xs[i]){
+        xj++ 
+      }
+      xs.remove(xv[xj])
+      yv2 = ys[xj]
+      ys.remove(yv[xj])
+      ymin = min(yv1, yv2)
+      ymax = max(yv1,yv2)
+      for(int i = ymin; i < (ymax + 1); i++){
+        setPixel(xv,i)
+      }
     }
   }
 
